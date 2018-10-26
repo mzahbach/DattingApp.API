e17daa229334f6caca616ca45905c0f5159fc3d9
bechir mzah <bechir.mzah@esprit.tn>
2018-10-26 15:26:11 +0100

    chap8 wfé

---
+++ DatingApp.API/Models/photo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DatingApp.API.Models
+{
+    public class photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+        public User User { get; set; }
+        public int userId { get; set; }
+    }
+}
